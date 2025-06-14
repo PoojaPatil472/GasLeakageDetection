5b10b60b97fa1fadd854ae15771547f1e09052b0
Pooja Patil <pp4933340@gmail.com>
2025-06-14 23:38:04 +0530

    ESP8266 NodeMCU controller Arduino code to detect gas leakage

---
+++ IOT_GAS_AduinoCode/IOT_GAS_AduinoCode.ino
@@ -0,0 +1,88 @@
+
+#include <ESP8266WiFi.h>
+const char* ssid = "P";    //  Your Wi-Fi Name
+const char* password = "whoareyou";   // Wi-Fi Password
+int BUZ_PIN = 2;   // led connected to GPIO2 (D4)
+int GAS_PIN = 14;
+
+WiFiServer server(80);
+
+void setup()
+{
+  Serial.begin(115200); //Default Baudrate
+  pinMode(BUZ_PIN, OUTPUT);
+  pinMode(GAS_PIN, INPUT);
+  digitalWrite(BUZ_PIN, LOW);
+  Serial.print("Connecting to the Newtork..");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected"); 
+  server.begin();  // Starts the Server
+  Serial.println("Server started");
+  Serial.print("IP Address of network: "); // will IP address on Serial Monitor
+  Serial.println(WiFi.localIP());
+  Serial.print("Copy and paste the following URL: http://"); // Will print IP address in URL format
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+}
+
+void loop()
+{
+  WiFiClient client = server.available();
+  if (!client)
+  {
+    return;
+  }
+  Serial.println("Waiting for new client");
+  while(!client.available())
+  {
+    delay(1);
+  }
+
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+  request.write();
+
+   /*int value = LOW;
+  if(request.indexOf("/LED=ON") != -1)
+  {
+    digitalWrite(LED, LOW); // Turn LED ON
+    value = LOW;
+  }
+
+  if(request.indexOf("/LED=OFF") != -1)
+  {
+    digitalWrite(LED, HIGH); // Turn LED OFF
+    value = HIGH;
+  }*/
+
+//*------------------HTML Page Code---------------------*//
+  client.println("HTTP/1.1 200 OK"); //
+  client.println("Content-Type: text/html");
+  client.println("");
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.print(" Gas Leakage Status: ");
+  if(digitalRead(GAS_PIN) == LOW)
+  {
+    client.print("DETECTED");
+  }
+  else
+  {
+    client.print("NORMAL");
+  }
+  client.println("<br><br>");
+  client.println("<a href=\"/LED=ON\"\"><button>ON</button></a>");
+  client.println("<a href=\"/LED=OFF\"\"><button>OFF</button></a><br />");
+  client.println("</html>");
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+}
