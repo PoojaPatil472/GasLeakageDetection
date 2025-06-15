667bf1f41d4e5da9714101d5ff8c6ea73909e135
PoojaPatil472 <pp4933340@gmail.com>
2025-06-15 05:52:34 +0530

    Corrected ESP8266 code

--- IOT_GAS_AduinoCode/IOT_GAS_AduinoCode.ino
+++ IOT_GAS_AduinoCode/IOT_GAS_AduinoCode.ino
@@ -4,17 +4,32 @@ const char* ssid = "P";    //  Your Wi-Fi Name
 const char* password = "whoareyou";   // Wi-Fi Password
 int BUZ_PIN = 2;   // led connected to GPIO2 (D4)
 int GAS_PIN = 14;
+int waterMotor=15;
+int Light=5;
 
+volatile bool gasDetected = false;
 WiFiServer server(80);
+
+/*void IRAM_ATTR buttonISR() {
+    gasDetected = true;
+    digitalWrite(BUZ_PIN,HIGH);
+    digitalWrite(waterMotor,HIGH);
+    digitalWrite(Light,LOW);
+}*/
 
 void setup()
 {
   Serial.begin(115200); //Default Baudrate
   pinMode(BUZ_PIN, OUTPUT);
-  pinMode(GAS_PIN, INPUT);
-  digitalWrite(BUZ_PIN, LOW);
+  pinMode(waterMotor, OUTPUT);
+  pinMode(GAS_PIN, INPUT_PULLUP);
+  pinMode(Light, OUTPUT);
+  digitalWrite(BUZ_PIN, LOW);//Initially keeping buzzer OFF
+  digitalWrite(waterMotor, LOW);//Initially keeping water motor OFF
+  digitalWrite(Light, HIGH);//Initially keeping lights ON
   Serial.print("Connecting to the Newtork..");
   WiFi.begin(ssid, password);
+  //attachInterrupt(digitalPinToInterrupt(GAS_PIN), buttonISR, FALLING);
 
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -34,42 +49,71 @@ void setup()
 void loop()
 {
   WiFiClient client = server.available();
-  if (!client)
+  if (client)
   {
-    return;
-  }
-  Serial.println("Waiting for new client");
-  while(!client.available())
-  {
-    delay(1);
-  }
+      Serial.println("Waiting for new client");
+      while(!client.available())
+      {
+        delay(1);
+      }
 
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
+      String request = client.readStringUntil('\r');
+      Serial.println(request);
+      client.flush();
+      
+      if(request.indexOf("/LED_ON") != -1)
+      {
+        digitalWrite(Light, HIGH); // Turn LED ON
+      }
 
-  request.write();
+      if(request.indexOf("/LED_OFF") != -1)
+      {
+        digitalWrite(Light, LOW); // Turn LED OFF
+      }
 
-//*------------------HTML Page Code---------------------*//
-  client.println("HTTP/1.1 200 OK"); //
-  client.println("Content-Type: text/html");
-  client.println("");
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
-  client.print(" Gas Leakage Status: ");
-  if(digitalRead(GAS_PIN) == LOW)
-  {
-    client.print("DETECTED");
+    //*------------------HTML Page Code---------------------*//
+      client.println("HTTP/1.1 200 OK"); //
+      client.println("Content-Type: text/html");
+      client.println("");
+      client.println("<!DOCTYPE HTML>");
+      client.println("<html>");
+      client.print(" Gas Leakage Status: ");
+
+      if(digitalRead(GAS_PIN) == LOW)
+      {
+        client.print("DETECTED");
+        digitalWrite(BUZ_PIN,HIGH);
+        digitalWrite(waterMotor,HIGH);
+        digitalWrite(Light,LOW);
+      }
+      else
+      {
+        client.print("NORMAL");
+        digitalWrite(BUZ_PIN,LOW);
+        digitalWrite(waterMotor,LOW);
+        //digitalWrite(Light,HIGH);
+      }
+      client.println("<br><br>");
+      client.println("<a href=\"/LED_ON\"\"><button>ON</button></a>");
+      client.println("<a href=\"/LED_OFF\"\"><button>OFF</button></a><br />");
+      client.println("</html>");
+      delay(1);
+      Serial.println("Client disonnected");
+      Serial.println("");
   }
   else
   {
-    client.print("NORMAL");
+      if(digitalRead(GAS_PIN) == LOW)
+      {
+        digitalWrite(BUZ_PIN,HIGH);
+        digitalWrite(waterMotor,HIGH);
+        digitalWrite(Light,LOW);
+      }
+      else
+      {
+        digitalWrite(BUZ_PIN,LOW);
+        digitalWrite(waterMotor,LOW);
+        //digitalWrite(Light,HIGH);
+      }
   }
-  client.println("<br><br>");
-  client.println("<a href=\"/LED=ON\"\"><button>ON</button></a>");
-  client.println("<a href=\"/LED=OFF\"\"><button>OFF</button></a><br />");
-  client.println("</html>");
-  delay(1);
-  Serial.println("Client disonnected");
-  Serial.println("");
 }
